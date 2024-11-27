af1dfb4f2410bb28f614e4996cfbae99d0e4b0db
Matheus Santos <matheus.santos008@edu.pucrs.br>
2024-11-27 16:39:22 -0300

    fix cors

--- services/user-service/api/src/main/java/com/pucrs/api/controllers/UserController.java
+++ services/user-service/api/src/main/java/com/pucrs/api/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pucrs.api.dtos.CreateUserDto;
@@ -13,6 +14,7 @@ import com.pucrs.api.services.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
